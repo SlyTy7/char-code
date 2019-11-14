c5b03c86ecf5cbec3cc0dbc89deeb9c60a66e00e
Tyler West <tylerwest@Tylers-Work-Macbook.local>
2019-11-14 11:35:44 -0800

    build LargeTile component

--- src/modules/LargeTile.js
+++ src/modules/LargeTile.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import '../css/LargeTile.css';
 
-function LargeTile() {
-  return (
-    <div className="large-tile">
-
-    </div>
-  );
+class LargeTile extends React.Component {
+  render() {
+    return (
+      <div className="large-tile">
+        <div className="large-tile-inner-container">
+          <p className="heading">Press any key to get the key info</p>
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default LargeTile;
