50fba6007a7f3a5c5c0ab0001bde7d0582d1a732
Tyler West <tyler.j.west.7@gmail.com>
2019-11-14 14:16:11 -0800

    make component conditional

--- src/modules/LargeTile.js
+++ src/modules/LargeTile.js
@@ -3,12 +3,25 @@ import '../css/LargeTile.css';
 
 export default class LargeTile extends React.Component {
   render() {
+    const keyObj = this.props.keyPressed;
+    const key = keyObj ? keyObj.char : "";
+    const keyCode = keyObj ? keyObj.charCode : "";
+    const code = keyObj ? keyObj.code : "";
+    const keyInfo = (
+      <div className="key-info">
+        <p className="code">{code}</p>
+        <p className="key">{key}</p>
+        <p className="key-code">{keyCode}</p>
+      </div>
+    );
+
+    const heading = <p className="heading">Press any key to get the key info</p>;
+    const tileBody = keyObj ? keyInfo : heading ;
+
     return (
       <div className="large-tile-container">
         <div className="large-tile">
-          <p className="heading">
-            {this.props.keyPressed ? this.props.keyPressed.char : "Press any key to get the key info"}
-          </p>
+          {tileBody}
         </div>
       </div>
     );
