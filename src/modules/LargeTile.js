4725b41e2041ab9ee712e84925e97ea2a59f0b3d
Tyler West <tylerwest@Tylers-Work-Macbook.local>
2019-11-14 12:06:09 -0800

    fix element class names

--- src/modules/LargeTile.js
+++ src/modules/LargeTile.js
@@ -4,8 +4,8 @@ import '../css/LargeTile.css';
 export default class LargeTile extends React.Component {
   render() {
     return (
-      <div className="large-tile">
-        <div className="large-tile-inner-container">
+      <div className="large-tile-container">
+        <div className="large-tile">
           <p className="heading">{this.props.currentKey || "Press any key to get the key info"}</p>
         </div>
       </div>
